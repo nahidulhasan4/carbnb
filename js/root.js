129e37faa9bc1f6cc756dc7ed7847bbbd3b280ae
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-01-11 11:42:48 +0600

    here add nevber transition when scroll y

--- js/root.js
+++ js/root.js
@@ -1,12 +1,11 @@
 
 window.onscroll = function () {
   var header = document.querySelector(".header");
-  var headerTop = document.querySelector(".header-top");
   if (window.scrollY > 0) {
     header.classList.add("sticky");
-    headerTop.style.display = "none";
+   
   } else {
     header.classList.remove("sticky");
-    headerTop.style.display = "block";
+  
   }
 };
